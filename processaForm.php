7abd535bc7e6cce632c4c828bf8996a9d9c842c7
Yu-Sant <kauanbrunocs@gmail.com>
2025-09-01 21:44:18 -0300

    Update processaForm.php

--- processaForm.php
+++ processaForm.php
@@ -1,5 +1,9 @@
 <?php
-$nome = $_POST["nome"];
-$email = $_POST["email"];
-echo "Sr. $nome, seu e-mail é $email";
-?>
+echo
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+  <meta charset="UTF-8">
+  <title>Form2<title>
+  </head>
+  
